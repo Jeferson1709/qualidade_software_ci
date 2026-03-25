74c12545cd816fb138c8740cea8a9494fde975e4
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-03-24 21:06:05 -0300

    feat: teste

--- src/main/java/com/example/educationalqualityproject/EducationalQualityProjectApplication.java
+++ src/main/java/com/example/educationalqualityproject/EducationalQualityProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EducationalQualityProjectApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(EducationalQualityProjectApplication.class, args);
     }
